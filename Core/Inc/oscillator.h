906ef6a690cee54ee123c9a99eb9d276597850f2
Michael Skiles <mjrskiles@gmail.com>
2022-01-04 20:15:29 -0600

    Started work on oscillator

---
+++ Core/Inc/oscillator.h
@@ -0,0 +1,12 @@
+//
+// Created by mjrsk on 1/3/2022.
+//
+
+#ifndef TS_DISCOVERY_OSCILLATOR_H
+#define TS_DISCOVERY_OSCILLATOR_H
+
+static float sample_rate_hz;
+
+//typedef struct
+
+#endif //TS_DISCOVERY_OSCILLATOR_H
